b4168b65bb48b270592ec591d0ad1de3f3e2e9e1
Juha <kukiyomenaisapp@gmail.com>
2023-07-26 22:43:46 +0900

    テストコード

--- SandBox/Program.cs
+++ SandBox/Program.cs
@@ -1,3 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿Console.WriteLine(GetMortonNumber(3, 6));
+Console.WriteLine(GetRoot(31, 19));
+Console.WriteLine(GetStartIndex(2));
 
-Console.WriteLine("Hello, World!");
+uint BitSeparate32(uint n)
+{
+    n = (n | (n << 8)) & 0x00ff00ff;
+    n = (n | (n << 4)) & 0x0f0f0f0f;
+    n = (n | (n << 2)) & 0x33333333;
+    return (n | (n << 1)) & 0x55555555;
+}
+
+uint GetMortonNumber(ushort x, ushort y)
+{
+    return BitSeparate32(x) | (BitSeparate32(y) << 1);
+}
+
+uint GetRoot(uint a, uint b)
+{
+    var shift = 0;
+
+    // 16
+    for (var i = 1; i < 16; i++)
+        if ((a ^ b) >> (i * 2) != 0)
+            shift = (i + 1) * 2;
+
+    return a >> shift;
+}
+
+int GetStartIndex(uint n)
+{
+    return ((int)Math.Pow(4, n) - 1) / 3;
+}
