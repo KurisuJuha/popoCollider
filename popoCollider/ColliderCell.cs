ab751d3ad442039e7772b0ff99f1d3ef13e8f9af
Juha <kukiyomenaisapp@gmail.com>
2023-07-29 18:03:33 +0900

    Create ColliderCell.cs

---
+++ popoCollider/ColliderCell.cs
@@ -0,0 +1,11 @@
+﻿namespace JuhaKurisu.PopoTools.ColliderSystem;
+
+public struct ColliderCell<T>
+{
+    public List<RectCollider<T>> Colliders;
+
+    public ColliderCell()
+    {
+        Colliders = new List<RectCollider<T>>();
+    }
+}
