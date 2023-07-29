a6d600568df95467686047e56ca3a67c20129889
Juha <kukiyomenaisapp@gmail.com>
2023-07-29 12:34:54 +0900

    RectCollider用のTransformを作成

---
+++ popoCollider/RectColliderTransform.cs
@@ -0,0 +1,17 @@
+﻿using JuhaKurisu.PopoTools.Deterministics;
+
+namespace JuhaKurisu.PopoTools.ColliderSystem;
+
+public struct RectColliderTransform
+{
+    public readonly FixVector2 Position;
+    public readonly FixVector2 Size;
+    public readonly Fix64 Angle;
+
+    public RectColliderTransform(FixVector2 position, FixVector2 size, Fix64 angle)
+    {
+        Position = position;
+        Size = size;
+        Angle = angle;
+    }
+}
