ce71c933e235de1a7c863973f4c774aac2ba2284
Juha <kukiyomenaisapp@gmail.com>
2023-07-29 18:57:19 +0900

    WorldTransformを追加

--- popoCollider/ColliderWorld.cs
+++ popoCollider/ColliderWorld.cs
@@ -1,16 +1,23 @@
+using JuhaKurisu.PopoTools.Deterministics;
+
 namespace JuhaKurisu.PopoTools.ColliderSystem;
 
 public class ColliderWorld<T>
 {
     private readonly ColliderCell<T>[] _colliderCells;
+    private readonly WorldTransform _worldTransform;
 
-    public ColliderWorld(ushort level)
+    public ColliderWorld(ushort level, Fix64 left, Fix64 top, Fix64 right, Fix64 bottom)
     {
         var length = ((int)Math.Pow(4, level + 1) - 1) / 3;
         _colliderCells = new ColliderCell<T>[length];
     }
 
     public void Register(RectCollider<T> collider)
+    {
+    }
+
+    private void CreateNewCell()
     {
     }
 }
---
+++ popoCollider/WorldTransform.cs
@@ -0,0 +1,9 @@
+using JuhaKurisu.PopoTools.Deterministics;
+
+namespace JuhaKurisu.PopoTools.ColliderSystem;
+
+public readonly struct WorldTransform
+{
+    public readonly FixVector2 Position;
+    public readonly FixVector2 Size;
+}
