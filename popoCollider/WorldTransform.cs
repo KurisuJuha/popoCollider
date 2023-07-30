4ed2d857340595878dd8e9c90b59d00a8716a759
Juha <kukiyomenaisapp@gmail.com>
2023-07-30 22:10:34 +0900

    WorldTransformにコンストラクタを追加

--- popoCollider/WorldTransform.cs
+++ popoCollider/WorldTransform.cs
@@ -6,4 +6,10 @@ public readonly struct WorldTransform
 {
     public readonly FixVector2 Position;
     public readonly FixVector2 Size;
+
+    public WorldTransform(FixVector2 position, FixVector2 size)
+    {
+        Position = position;
+        Size = size;
+    }
 }
