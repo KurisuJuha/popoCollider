90f67bbd8f5d0bd9a2594a508e1a6f642befa1fe
Juha <kukiyomenaisapp@gmail.com>
2023-08-01 21:22:41 +0900

    Colliderのコンストラクタの引数にworldを追加

--- popoCollider/RectCollider.cs
+++ popoCollider/RectCollider.cs
@@ -4,14 +4,19 @@ namespace JuhaKurisu.PopoTools.ColliderSystem;
 
 public readonly struct RectCollider<T>
 {
-    public readonly AABB AABB;
+    private readonly AABB AABB;
     public readonly T Entity;
     public readonly RectColliderTransform Transform;
+    private readonly ColliderWorld<T> World;
+    public readonly long index;
 
-    public RectCollider(T entity, RectColliderTransform transform)
+    public RectCollider(T entity, RectColliderTransform transform, ColliderWorld<T> world)
     {
         Entity = entity;
         Transform = transform;
         AABB = new AABB(transform);
+        World = world;
+        index = GetIndex();
+    }
     }
 }
