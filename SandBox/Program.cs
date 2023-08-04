94c96869dceca1cb9cfc17eb39701261e51cd432
Juha <kukiyomenaisapp@gmail.com>
2023-08-05 04:44:08 +0900

    Update Program.cs

--- SandBox/Program.cs
+++ SandBox/Program.cs
@@ -1,11 +1,6 @@
 ﻿using JuhaKurisu.PopoTools.ColliderSystem;
 using JuhaKurisu.PopoTools.Deterministics;
 
-var aabb0 = new AABB(new RectColliderTransform(FixVector2.one, FixVector2.one, Fix64.zero));
-Console.WriteLine($"{aabb0.LeftTopPosition}, {aabb0.RightBottomPosition}");
-var aabb1 = aabb0.Rescale(new WorldTransform(16, FixVector2.zero, new FixVector2(10, 10)));
-Console.WriteLine($"{aabb1.LeftTopPosition}, {aabb1.RightBottomPosition}");
-
 var colliderWorld = new ColliderWorld<int>(new WorldTransform(8, new FixVector2(0, 0), new FixVector2(10000, 10000)));
 
 const int count = 10000000;
@@ -14,6 +9,8 @@ for (var i = 0; i < colliders.Length; i++)
     colliders[i] = new RectCollider<int>(10,
         new RectColliderTransform(new FixVector2(10000, 10000), FixVector2.zero, Fix64.zero),
         colliderWorld);
+
+Console.ReadKey(true);
 
 var startTime = DateTime.Now;
 for (var i = 0; i < colliders.Length; i++)
